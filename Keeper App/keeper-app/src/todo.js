5011e12743ee9a16efb76ae4ef58e7dce9f354c9
Josephine Gyamera <test@TESTs-MacBook-Pro.local>
2020-03-29 00:59:56 +0200

    Complete version

---
+++ Keeper App/keeper-app/src/todo.js
@@ -0,0 +1,21 @@
+
+
+const todo = [
+    {
+        id:1,
+        title:"Sleep",
+        content:"I will probably sleep"
+    },
+    {
+        id:2,
+        title:"Sleep",
+        content:"I will probably sleep"
+    },
+    {
+        id:3,
+        title:"Sleep",
+        content:"I will probably sleep"
+    }
+]
+
+export default todo;
